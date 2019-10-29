7cadd284b14eb6d14f2502a491c51098e50b27da
Ercan-home <ercanerdogan@gmail.com>
2019-10-29 23:15:34 +0300

    interface and polymorphism usage

---
+++ IEater/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEater
+{
+    class Program
+    {
+
+        interface IEater
+        {
+            void Eat();
+        }
+
+        class Dog : IEater
+        {
+            public void Eat()
+            {
+                Console.WriteLine($"{nameof(Dog)} is eating");
+            }
+        }
+
+        class Cat : IEater
+        {
+            public void Eat()
+            {
+                Console.WriteLine($"{nameof(Cat)} is eating");
+            }
+            public void Meow()
+            {
+                Console.WriteLine($"{nameof(Cat)} is miyav");
+            }
+        }
+
+        private static void Eat(IEater eater)
+        {
+            eater.Eat();
+        }
+
+        static void Main(string[] args)
+        {
+            IEater cat = new Cat();
+            Eat(cat);
+
+            IEater dog = new Dog();
+            Eat(dog);
+
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
